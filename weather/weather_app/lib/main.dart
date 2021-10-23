0a0c1bd666a0100a2f91ba66dde31b7af596130e
hoiboi <gentledemeiz@gmail.com>
2021-10-23 23:07:36 +0300

    bg + title 10c

--- weather/weather_app/lib/main.dart
+++ weather/weather_app/lib/main.dart
@@ -5,20 +5,32 @@ void main() {
   runApp(Main());
 }
 
-class Main extends StatelessWidget{
+class Main extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/bg.png"),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/images/bg.png"),
               fit: BoxFit.cover,
-            )
+            )),
+            child: Center(
+              child: Text(
+                "10°с",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Gilroy-medium",
+                  fontSize: 80,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+             ), padding: EdgeInsets.fromLTRB(0, 0, 0, 650),
           ),
-        )
-      )
-    );
+          ),
+        );
   }
 }
