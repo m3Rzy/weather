4846fd40cff0a5c2b5a895fb8f999265b4fee12f
hoiboi <gentledemeiz@gmail.com>
2021-10-26 12:41:01 +0300

    убрать кнопку + сделать боттом-щит + убрать контейнер с отступами и заменить на виджет padding

--- weather/weather_app/lib/main.dart
+++ weather/weather_app/lib/main.dart
@@ -10,7 +10,7 @@ class Main extends StatelessWidget {
     String colornew = '0xff' + colorhexcode;
     colornew = colornew.replaceAll('#', '');
     int colorint = int.parse(colornew); //метод для цветов с кодом
-    return colorint;  //для кнопки было
+    return colorint; //для кнопки было
   }
 
   @override
@@ -18,42 +18,77 @@ class Main extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage("assets/images/bg.png"),
-            fit: BoxFit.cover,
-          )),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.fromLTRB(0, 70, 0, 0), //мб удалить контейнер с отступом - узнать у препода
+        body: Builder(
+          builder: (context) {
+            return Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("assets/images/bg.png"),
+                fit: BoxFit.cover,
+              )),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 70, 0,
+                        0), //мб удалить контейнер с отступом - узнать у препода
+                  ),
+
+
+                ElevatedButton(
+                  child: Text('vlad love'),
+                onPressed: () {
+                  print('test');
+                  showModalBottomSheet<void>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Container(
+                          height: 360,
+                          color: Colors.amber,
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                const Text('Modal BottomSheet'),
+                                ElevatedButton(
+                                  child: const Text('Close BottomSheet'),
+                                  onPressed: () => Navigator.pop(context),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                      );
+                },
               ),
-              Text(
-                "10°с",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "Gilroy-medium",
-                  fontSize: 80,
-                  fontWeight: FontWeight.w600,
-                ),
+
+                  const Text(
+                    "10°с",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Gilroy-medium",
+                      fontSize: 80,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Text(
+                    "23 сент. 2021",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Gilroy-medium",
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                "23 сент. 2021",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "Gilroy-medium",
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
+            );
+          }
         ),
       ),
     );
   }
 }
-
