6716b51081b0a8508e7c4463bc41546318b9a0ed
hoiboi <gentledemeiz@gmail.com>
2021-10-27 11:06:25 +0300

    добавил bottom sheet, сделать: в нем компоненты и оформить главную страницу

--- weather/weather_app/lib/main.dart
+++ weather/weather_app/lib/main.dart
@@ -10,44 +10,63 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage("assets/images/bg.png"),
-            fit: BoxFit.cover,
-          )),
-          child: Center(
-            child: Column(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 70, 0, 0),
-                  child: Text(
-                    "10°с",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "Gilroy-medium",
-                      fontSize: 80,
-                      fontWeight: FontWeight.w600,
+      home: Builder(builder: (context) {
+        Future.delayed(Duration.zero, () {
+          showModalBottomSheet(
+            enableDrag: false,
+            backgroundColor: const Color(0xffE2EBFF),
+            barrierColor: Colors.transparent,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+            ),
+            context: context,
+            builder: (context) => Container(
+              height: 256,
+            ),
+          );
+        });
+        return Scaffold(
+          body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/images/bg.png"),
+              fit: BoxFit.cover,
+            )),
+            child: SafeArea(
+              child: Center(
+                child: Column(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 39, 0, 0),
+                      child: Text(
+                        "10°с",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Gilroy-medium",
+                          fontSize: 80,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
                     ),
-                  ),
+                    Text(
+                      "23 сент. 2021",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Gilroy-medium",
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  "23 сент. 2021",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: "Gilroy-medium",
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
