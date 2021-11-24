e1ec2c0f539e975c4af5ff11080aad39fb00f383
hoiboi <gentledemeiz@gmail.com>
2021-11-25 02:20:30 +0300

    как сделать центр на странице: о разрабе

--- weather/weather_app/lib/about.dart
+++ weather/weather_app/lib/about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/_mainscreen.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -9,34 +10,61 @@ class AboutPage extends StatelessWidget {
     return Material(
       child: Container(
         color: const Color(0xffE2EBFF),
-        padding: const EdgeInsets.fromLTRB(0, 0, 139, 600),
-        child: InkWell(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icon(
-                  Icons.keyboard_arrow_left,
-                  size: 29.0,
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 20.0),
-                  child: Text(
-                    'О разработчике',
-                    style: TextStyle(
-                        fontFamily: "Gilroy-medium",
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 60, 0, 0),
+              child: InkWell(
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.keyboard_arrow_left,
+                        size: 29.0,
+                        color: Colors.black,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text(
+                          'О разработчике',
+                          style: TextStyle(
+                              fontFamily: "Gilroy-medium",
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const MainScreenPage()));
+                  }),
             ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const MainScreenPage()));
-            }),
+            SizedBox(
+              ///// КАК СДЕЛАТЬ В ЦЕНТРЕ ТВОЮ МАТЬ
+              width: 255,
+              height: 52,
+              child: Neumorphic(
+                style: NeumorphicStyle(
+                    shape: NeumorphicShape.concave,
+                    boxShape:
+                        NeumorphicBoxShape.roundRect(BorderRadius.circular(15)),
+                    lightSource: LightSource(0, -4),
+                    depth: -16,
+                    intensity: 0.5,
+                    color: Color(0xffE2EBFF)),
+                child: const Center(
+                  child: Text('Weather app',
+                      style: TextStyle(
+                          fontFamily: "Gilroy-medium",
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w800)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
