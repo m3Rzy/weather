1f81b4d4537f8e394efedda6992cf673b785c16d
hoiboi <gentledemeiz@gmail.com>
2021-11-30 12:16:50 +0300

    норм поиск сделал, кроме кнопки cancel

--- weather/weather_app/lib/search.dart
+++ weather/weather_app/lib/search.dart
@@ -9,8 +9,38 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: TextField(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 60, 0, 0),
+              child: Row(
+                children: [
+                  IconButton(onPressed: () { Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const MainScreenPage()));},
+                  icon: const Icon(
+                    Icons.keyboard_arrow_left,
+                    size: 29.0,
+                    color: Colors.black,
+                  ),),
+                  Container(
+                    width: 300,
+                    height: 50,
+                    child: const TextField(
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        hintText: 'Введите название города',
+                      ),
+                    ),
+                  ),
+                  IconButton(onPressed: () { print('dsa');}, icon: const Icon(Icons.cancel, size: 20,) )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
